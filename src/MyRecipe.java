7e13efba118d90064880d71df7805118d51cb69a
student <@bths.edu>
2023-05-23 10:18:43 -0400

    hi

---
+++ src/MyRecipe.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MyRecipe {
+    private JLabel MyRecipe;
+    private JTextField textField1;
+    private JButton enterButton;
+    private JButton clearButton;
+    private JButton beefButton;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/Recipe.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Recipe {
+    private String title;
+    private ArrayList<String> ingredients;
+
+    public Recipe(String name, ArrayList<String> i) {
+        title = name;
+        ingredients = i;
+    }
+
+    //returns the name of the recipe
+    public String getTitle(){
+        return title;
+    }
+
+    //returns arraylist of all ingredients
+    public ArrayList<String> getIngredients(){
+        return ingredients;
+    }
+
+    //returns ingredient of a certain index
+    public String getSpecficIngredient(String find){
+        for(int i = 0; i < ingredients.size(); i++){
+            if (ingredients.get(i).equals(find)){
+                return ingredients.get(i);
+            }
+        }
+        return find;
+    }
+}

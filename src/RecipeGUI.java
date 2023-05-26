99c2fe005dbf4e62234565a80769e8e41988411d
student <@bths.edu>
2023-05-26 10:59:56 -0400

    hi

--- src/RecipeGUI.java
+++ src/RecipeGUI.java
@@ -1,2 +1,42 @@
-public class RecipeGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RecipeGUI extends JFrame implements ActionListener {
+
+    private JLabel meal;
+    private JTextField instructionsLabel;
+    private JButton backButton;
+    private JPanel meaPanel;
+    private JTextField ingredientsTextField;
+    private JPanel instructionsPanel;
+    private JPanel backPanel;
+
+    public RecipeGUI() {
+        createUIComponents();
+    }
+
+    private void createUIComponents() {
+        setContentPane(meaPanel);
+        setTitle("Recipe");
+        setSize(1050, 550);
+        setLocation(450, 100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //sets up listeners
+        backButton.addActionListener(this);
+        setVisible(false);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object actionSource = e.getSource();
+        if (actionSource instanceof JButton) {
+            JButton button = (JButton) actionSource;
+            if (button.getText().equals("Back")) {
+
+            }
+
+        }
+    }
+
 }
